31afd0c24ccc33d07b28538ea55570a3040f6546
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-04-24 21:35:49 +0500

    updated the database

--- store-app/src/app/app.routes.ts
+++ store-app/src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
         path: 'home',
         component: HomeComponent,
 
-        path: 'pageNotFound',
-        component: ErrorPage,
+        // path: 'pageNotFound',
+        // component: ErrorPage,
     },
 ];
